4573fa8f1d1f393bea1ff0147ef32a6f4f6ad57d
andrecarrsd2008 <andrecarrsd2008@gmail.com>
2018-11-21 22:31:48 -0800

    Completed Node.js Rest API w/Restify, Mongoose

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ module.exports = {
   ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3000,
   URL: process.env.BASE_URL || 'http://localhost:3000',
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://abc123!:abc123!@ds113134.mlab.com:13134/customer_api'
+  MONGODB_URI: process.env.MONGODB_URI || 
+  'mongodb://abc123!:abc123!@ds113134.mlab.com:13134/customer_api',
+  JWT_SECRET: process.env.JWT_SECRET || 'secret1'
 }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const errors = require('restify-errors');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const auth = require('../auth');
+const config = require('../config');
 
 module.exports = server => {
  // Register User
@@ -26,6 +29,29 @@ module.exports = server => {
         }
       });
     });
-
   });
-}
+
+  // Auth User
+  server.post('/auth', async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+      // Authenticate User
+      const user = await auth.authenticate(email, password); 
+      
+     // Create JWT
+     const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {
+       expiresIn: '15m'
+     });
+
+     const { iat, exp } = jwt.decode(token);
+     // Respond With Token
+     res.send({ iat, exp, token });
+
+      next();
+    } catch (err) {
+      // User Unauthorized
+      return next(new errors.UnauthorizedError(err));
+    }
+  });
+};
